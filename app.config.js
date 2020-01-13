5753df4435ed44df7c4e99aaa84f5c79a3ba2069
bobo <gucb@253.com>
2020-01-13 15:28:03 +0800

    all

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: "创蓝智能云平台",
-  publicPath: "app1", //根目录名称
+  title: "智能云平台",
+  publicPath: "<%= appName %>", //根目录名称
   port: 9001,
   autoOpen: false, //启动本地是否自动重启,默认不启动
   alias: {}, //目录别名如"@"表示src目录,"components"表示src/components目录，"pages"表示src/pages,"assets"表示src/assets
